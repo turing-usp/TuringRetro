c2a989e7c221eb7461b28379b838520eba5126e4
Nelson Alves <nelsonayamashita@gmail.com>
2021-03-17 00:19:03 -0300

    :sparkles: Adiciona configurações para Impala

--- run_rllib.py
+++ run_rllib.py
@@ -5,7 +5,7 @@ import retro
 from utils import retro_wrappers
 
 import ray
-from ray.rllib.agents.ppo import PPOTrainer, DEFAULT_CONFIG
+from ray.rllib.agents import ppo, impala
 from utils.rllib_utils import register_retro, train, test
 
 parser = argparse.ArgumentParser()
@@ -13,6 +13,8 @@ parser.add_argument("game", type=str)
 parser.add_argument("state", type=str)
 parser.add_argument("-c", "--checkpoint", type=str)
 parser.add_argument("-t", "--train", action="store_true")
+parser.add_argument("-a", "--agent", type=str, default="PPO")
+parser.add_argument("-f", "--framework", type=str, default="tf")
 parser.add_argument("-e", "--episodes", type=int, default=1)
 
 args = parser.parse_args()
@@ -23,35 +25,52 @@ if __name__ == "__main__":
     wrapper = retro_wrappers.get_wrapper(game)
     checkpoint = args.checkpoint
     training = args.train
+    agent = args.agent
+    framework = args.framework
     episode_count = args.episodes
 
     info = ray.init(ignore_reinit_error=True)
-
-    trainer_config = DEFAULT_CONFIG.copy()
-    trainer_config['log_level'] = "WARN"
-    trainer_config['framework'] = 'tf'
-    trainer_config['lambda'] = 0.95
-    trainer_config['kl_coeff'] = 0.5
-    trainer_config['clip_rewards'] = True
-    trainer_config['clip_param'] = 0.1
-    trainer_config['vf_clip_param'] = 10.0
-    trainer_config['entropy_coeff'] = 0.01
-    trainer_config['num_workers'] = 0
-    trainer_config["num_cpus_per_worker"] = 4
-    trainer_config["num_envs_per_worker"] = 1
-    trainer_config["num_gpus"] = 1
-    trainer_config["num_gpus_per_worker"] = 1
-    trainer_config["train_batch_size"] = 500
-    trainer_config['rollout_fragment_length'] = 100
-    trainer_config['sgd_minibatch_size'] = 128
-    trainer_config['num_sgd_iter'] = 10
-    trainer_config['batch_mode'] = "truncate_episodes"
-    trainer_config['observation_filter'] = "NoFilter"
-    trainer_config['model']['vf_share_layers'] = True
-    trainer_config['output'] = './checkpoints/'
-
+    
     register_retro(game, state, wrapper)
-    agent = PPOTrainer(config=trainer_config, env=game)
+    
+    if agent == "PPO":
+        trainer_config = ppo.DEFAULT_CONFIG.copy()
+        trainer_config['num_workers'] = 0
+        trainer_config["num_cpus_per_worker"] = 4
+        trainer_config["num_envs_per_worker"] = 1
+        trainer_config['lambda'] = 0.95
+        trainer_config['kl_coeff'] = 0.5
+        trainer_config['clip_param'] = 0.1
+        trainer_config['vf_clip_param'] = 10.0
+        trainer_config['entropy_coeff'] = 0.01
+        trainer_config["train_batch_size"] = 500
+        trainer_config['rollout_fragment_length'] = 100
+        trainer_config['sgd_minibatch_size'] = 128
+        trainer_config['num_sgd_iter'] = 10
+        trainer_config['batch_mode'] = "truncate_episodes"
+        trainer_config['observation_filter'] = "NoFilter"
+        agent = ppo.PPOTrainer(config=trainer_config, env=game)
+    elif agent == "IMPALA":
+        trainer_config = impala.DEFAULT_CONFIG.copy()
+        trainer_config['rollout_fragment_length'] = 20
+        trainer_config['train_batch_size'] = 32
+        trainer_config['num_envs_per_worker'] = 1
+        trainer_config['grad_clip'] = 40
+        trainer_config['momentum'] = 0.00
+        trainer_config['epsilon'] = 0.01
+        agent = impala.ImpalaTrainer(config=trainer_config, env=game)
+    
+    #Common parameters
+    trainer_config['log_level'] = "WARN"
+    trainer_config['clip_rewards'] = True
+    trainer_config["num_gpus"] = 1
+    
+    if framework == "torch": 
+        trainer_config['framework'] = 'torch'
+    else:
+        trainer_config['framework'] = 'tf'
+        
+    trainer_config['output'] = './checkpoints/'    
 
     if training:
         trainer = train(agent, checkpoint=checkpoint)
