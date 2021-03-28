ead45017b22ef9214b2fef88cd12a518863f97bf
Nelson Alves <nelsonayamashita@gmail.com>
2021-03-28 17:36:02 -0300

    :bug: Arruma arquivos de configuração

--- run_rllib.py
+++ run_rllib.py
@@ -5,7 +5,7 @@ import retro
 from utils import retro_wrappers
 
 import ray
-from ray.rllib.agents import ppo, impala
+from ray.rllib.agents import ppo, impala, dqn
 from utils.rllib_utils import register_retro, train, test
 
 parser = argparse.ArgumentParser()
@@ -49,26 +49,41 @@ if __name__ == "__main__":
         trainer_config['num_sgd_iter'] = 10
         trainer_config['batch_mode'] = "truncate_episodes"
         trainer_config['observation_filter'] = "NoFilter"
+        trainer_config['framework'] = 'tf' if framework == "tf" else 'torch'
+            
         agent = ppo.PPOTrainer(config=trainer_config, env=game)
+    elif agent == "APEXDQN":
+        trainer_config = dqn.apex.APEX_DEFAULT_CONFIG.copy()
+        trainer_config['log_level'] = "WARN"
+        trainer_config['clip_rewards'] = True
+        trainer_config["num_gpus"] = 1
+        trainer_config['output'] = './checkpoints/' 
+        trainer_config['target_network_update_freq'] = 20000
+        trainer_config["remote_worker_envs"] = True
+        trainer_config["num_workers"] = 4
+        trainer_config['num_envs_per_worker'] = 2
+        trainer_config['lr'] = .00005
+        trainer_config['train_batch_size'] = 64
+        trainer_config['gamma'] = 0.99
+        agent = dqn.apex.ApexTrainer(config=trainer_config, env=game)
     elif agent == "IMPALA":
         trainer_config = impala.DEFAULT_CONFIG.copy()
+        trainer_config['log_level'] = "WARN"
+        trainer_config['clip_rewards'] = True
+        trainer_config["num_gpus"] = 1
+        trainer_config['output'] = './checkpoints/' 
         trainer_config['rollout_fragment_length'] = 50
         trainer_config['train_batch_size'] = 500
+        trainer_config["remote_worker_envs"] = True
         trainer_config['num_workers'] = 8
-        trainer_config['num_envs_per_worker'] = 1
-        agent = impala.ImpalaTrainer(config=trainer_config, env=game)
-    
-    #Common parameters
-    trainer_config['log_level'] = "WARN"
-    trainer_config['clip_rewards'] = True
-    trainer_config["num_gpus"] = 1
-    
-    if framework == "torch": 
-        trainer_config['framework'] = 'torch'
-    else:
-        trainer_config['framework'] = 'tf'
+        trainer_config['num_envs_per_worker'] = 4
+        trainer_config['lr_schedule'] = [           
+                [0, 0.0005],
+                [20000000, 0.000000000001],
+        ]
+        trainer_config['framework'] = 'tf' if framework == "tf" else 'torch'
         
-    trainer_config['output'] = './checkpoints/'    
+        agent = impala.ImpalaTrainer(config=trainer_config, env=game) 
 
     if training:
         trainer = train(agent, checkpoint=checkpoint)
